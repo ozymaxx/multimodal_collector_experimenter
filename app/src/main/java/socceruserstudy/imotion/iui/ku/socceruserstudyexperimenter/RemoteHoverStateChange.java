9967f4a4bc9da2cd094b7556b8934ba37515585b
Ozan Can Altiok <ozymaxx@gmail.com>
2016-09-30 17:11:56 +0300

    remote pointer support added

---
+++ app/src/main/java/socceruserstudy/imotion/iui/ku/socceruserstudyexperimenter/RemoteHoverStateChange.java
@@ -0,0 +1,22 @@
+package socceruserstudy.imotion.iui.ku.socceruserstudyexperimenter;
+
+/**
+ * Created by ozymaxx on 23.07.2016.
+ */
+
+public class RemoteHoverStateChange implements Runnable {
+    private CanvasView delegatedCanvas;
+    private boolean remoteHovered;
+    private String receivedContent;
+
+    public RemoteHoverStateChange(CanvasView canvas, boolean remoteHovered,String receivedContent) {
+        this.delegatedCanvas = canvas;
+        this.remoteHovered = remoteHovered;
+        this.receivedContent = receivedContent;
+    }
+
+    @Override
+    public void run() {
+        delegatedCanvas.setRemoteHovered(remoteHovered,receivedContent);
+    }
+}
